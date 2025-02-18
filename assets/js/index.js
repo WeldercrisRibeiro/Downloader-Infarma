ed70fcea27b26041ff4e78ff35d5ceb59bd2f87a
Weldercris Ribeiro <weldercris.ribeiro.dev@gmail.com>
2025-02-18 17:02:40 -0300

    feat: incluindo saida

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,23 @@
 function entrar(){
-    let usuario = document.getElementById('usuario').value
-    let senha = document.getElementById('password').value
+    let usuarios = [
+        { usuario: "weldercris.ribeiro@infarma.com.br", senha: "@Cris2507" },
+        { usuario: "rodrigo.almeida@infarma.com.br", senha: "Rudrigo252" },
+        { usuario: "kelton.lima@infarma.com.br", senha: "k.lima252" }
+    ];
+    let usuarioDigitado = document.getElementById('usuario').value;
+    let senhaDigitada = document.getElementById('password').value;
 
-    if (usuario === 'admin' && senha === 'senha'){
-        return window.location.href='main.html';
+    let usuarioValido = usuarios.some(user => user.usuario === usuarioDigitado && user.senha === senhaDigitada);
+
+    if (usuarioValido) {
+        window.location.href = 'main.html';
     } else {
-        alert('Usuário ou senha incorretos!')
+        alert('Usuário ou senha incorretos!');
     }
 }
+
+function sair() {
+    sessionStorage.removeItem("usuarioLogado");
+    window.location.href = 'index.html'; // Volta para a tela de login
+}
+

09949e3748d58e9fb3a398a1a68a9649698bd29f
Weldercris Ribeiro <weldercris.ribeiro.dev@gmail.com>
2025-10-28 21:30:15 -0300

    producao ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,119 @@
-import users from './versoes.js';
-
 // Bloqueia acesso se não estiver logado
 if (sessionStorage.getItem("loggedIn") !== "true") {
     window.location.href = "index.html";
 }
 
 // Mapeamento das versões e seus executáveis
+const versoes = {
+    "2008a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2102a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2202a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2202aa":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2202ac": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB"],
+	"2202ad":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2202ae":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2202af":["VmdRrd.CAB", "VmdPdv.CAB"],
+	"2202ag":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+    
+	"2205a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2205aa":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2205ab":["VmdPdv.CAB"],
+	"2205b":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2205ba":["VmdRrd.CAB"],
+	"2205c":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2208a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2208ba":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2208c":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2208ca":["VmdRrd.CAB"],
+	"2208ce":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2208d":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2208da":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2208db":["VmdRrd.CAB"],
+	"2208dc":["VmdPdv.CAB"],
+	"2211a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2211aa":["VmdRrd.CAB"],
+	"2211b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2211ba":["VmdTrm.CAB"],
+	"2211bb":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2211bc":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	
+	"2304ac": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2304b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2304ba":["VmdRrd.CAB","VmdTrm.CAB"],
+	"2304bb":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2304bc":["VmdRrd.CAB", "VmdPdv.CAB"],
+	
+	"2306a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2306b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2306ba":["VmdRrd.CAB", "VmdTrm.CAB"],
+	
+	"2308a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2308aa": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2308ab": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2308b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2308c":["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2308ca":["VmdPdv.CAB"],
+	"2308cb":["VmdRrd.CAB","VmdPdv.CAB"],
+	"2308cd":["VmdRrd.CAB"],
+	
+	"2312a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2312aa":["VmdRrd.CAB"],
+	"2312b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2312ba": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	
+	"2402a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	
+	"2402c": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2402d": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2402e": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	
+	"2403a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+    "2403b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2403c": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2403cc": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2403cd": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2403ce": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2403cf": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2403cg": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2403ch": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	
+	"2407a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2407aa": ["VmdRrd.CAB"],
+	"2407ab": ["VmdRrd.CAB"],
+	
+	"2411a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2411aa": ["VmdRrd.CAB", "VmdImp.CAB"],
+	"2411ab": ["VmdRrd.CAB"],
+	
+	"2411b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2411ba": ["VmdPdv.CAB"],
+	"2411bb": ["VmdTrm.CAB","VmdPdv.CAB"],
+	"2411bc": ["VmdRrd.CAB"],
+	"2411bd": ["VmdRrd.CAB","VmdPdv.CAB"],
+	"2411c": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2411ca": ["VmdPdv.CAB"],
+	"2411cb": ["VmdPdv.CAB"],
 
+	
+	
+	"2502a": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2502aa": ["VmdRrd.CAB",  "VmdPdv.CAB"],
+	
+	"2502b": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2502bc": ["VmdPdv.CAB"],
+	"2502bd": ["VmdPdv.CAB"],
+	"2502db": ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2502dd": ["VmdTrm.CAB","VmdPdv.CAB"],
+	"2502de": ["VmdPdv.CAB"],
+	"2502e":  ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB","VmdImp.CAB","VmdRljImp.CAB","VmdSrv.CAB"],
+	"2502ea": ["VmdRrd.CAB"],
+	"2502eb": ["VmdRrd.CAB"],
+	"2502ec":  ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2502f":  ["VmdRrd.CAB", "VmdTrm.CAB", "VmdPdv.CAB"],
+	"2502fa":  ["VmdTrm.CAB", "VmdPdv.CAB"]
+	
+	
+};
 
 // Atualiza os executáveis disponíveis conforme a versão
 function atualizarExecutaveis() {

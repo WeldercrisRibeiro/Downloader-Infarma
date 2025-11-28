99a48ceaa2e3a0546852153090525761a75d49de
Weldercris Ribeiro <weldercris.ribeiro.dev@gmail.com>
2025-11-28 16:38:08 -0300

    feat: add folder versions

--- assets/js/docs-list.js
+++ assets/js/docs-list.js
@@ -1,63 +1,160 @@
 // assets/js/docs-list.js
 
 export const listaDocumentos = [
+  // --- GERAL ---
+  // {
+  //     id: 'geral_01',
+  //     categoria: 'Geral',
+  //     titulo: 'Perguntas Frequentes',
+  //     arquivo: 'faq.md', // Arquivo na raiz do docs
+  //     descricao: 'Dúvidas comuns.'
+  // }
+  // --- DOCS VERSÃO ---
 
-            // --- GERAL ---
-    // {
-    //     id: 'geral_01',
-    //     categoria: 'Geral',
-    //     titulo: 'Perguntas Frequentes',
-    //     arquivo: 'faq.md', // Arquivo na raiz do docs
-    //     descricao: 'Dúvidas comuns.'
-    // }
-// --- DOCS VERSÃO ---
+  //25.02
+  {
+    id: "doc2502_01",
+    categoria: "Documentos de Versões",
+    titulo: "Versão 25.02a - Suporte",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.02/25.02a",
+    arquivo: "versoes/25.02/25.02a/pdfs/25.02a_Suporte.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
+
+  {
+    id: "doc2502_02",
+    categoria: "Documentos de Versões",
+    titulo: "Versão 25.02a - Clientes",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.02/25.02a",
+    arquivo: "versoes/25.02/25.02a/pdfs/25.02a_Clientes.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
+
+  {
+    id: "doc2502_03",
+    categoria: "Documentos de Versões",
+    titulo: "Leia-Me - SUPORTE",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.02/25.02a",
+    arquivo: "versoes/25.02/25.02a/pdfs/Leia-Me - SUPORTE.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
+
     {
-        id: 'doc_01',
-        categoria: 'Documentos de Versões',
-        titulo: 'Versão 25.03a - Suporte',
-        tipo: 'pdf', // <--- DEFINE COMO MARKDOWN
-        arquivo: 'versoes/25.03a/pdfs/25.03a_Suporte.pdf', 
-        //descricao: 'Como dar entrada via XML.'
-    },
+    id: "doc2502_04",
+    categoria: "Documentos de Versões",
+    titulo: "Anexo I - Balanço de Itens de controle especial e antimicrobianos",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.02/25.02a",
+    arquivo: "versoes/25.02/25.02a/pdfs/Anexo I - Balanço de Itens de controle especial e antimicrobianos.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
 
-        {
-        id: 'doc_02',
-        categoria: 'Documentos de Versões',
-        titulo: 'Versão 25.03a - Clientes',
-        tipo: 'pdf', // <--- DEFINE COMO MARKDOWN
-        arquivo: 'versoes/25.03a/pdfs/25.03a_Clientes.pdf', 
-        //descricao: 'Como dar entrada via XML.'
-    },
-
-    // --- MÓDULO ESTOQUE ---
     {
-        id: 'est_01',
-        categoria: 'Módulo Estoque',
-        titulo: 'Entrada de Nota Fiscal',
-        tipo: 'md', // <--- DEFINE COMO MARKDOWN
-        arquivo: 'estoque/manual_entradaNFe.md', 
-        descricao: 'Como dar entrada via XML.'
-    },
+    id: "doc2502_05",
+    categoria: "Documentos de Versões",
+    titulo: "Anexo II - Conciliação de cartões - Boa Vista_Atualização v.2502",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.02/25.02a",
+    arquivo: "versoes/25.02/25.02a/pdfs/Anexo II - Conciliação de cartões - Boa Vista_Atualização v.2502.pdf",
+    //descricao: 'Como dar entrada via XML.' Anexo III - Nova estrutura do cadastro de Endereços.pdf
+  },
 
-    // --- MÓDULO VENDAS ---
     {
-        id: 'vnd_01',
-        categoria: 'Módulo Vendas',
-        titulo: 'Política de Vendas',
-        tipo: 'md',
-        arquivo: 'vendas/politica_venda.md',
-        descricao: 'Regras de descontos e prazos.'
-    },
+    id: "doc2502_06",
+    categoria: "Documentos de Versões",
+    titulo: "Anexo III - Nova estrutura do cadastro de Endereços",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.02/25.02a",
+    arquivo: "versoes/25.02/25.02a/pdfs/Anexo III - Nova estrutura do cadastro de Endereços.pdf",
+    //descricao: 'Como dar entrada via XML.' Anexo III - Nova estrutura do cadastro de Endereços.pdf
+  },
 
 
-        // --- MÓDULO FINANCEIRO ---
+  //25.03
+  {
+    id: "doc2503_01",
+    categoria: "Documentos de Versões",
+    titulo: "Versão 25.03a - Suporte",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.03/25.03a",
+    arquivo: "versoes/25.03/25.03a/pdfs/25.03a_Suporte.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
+
+  {
+    id: "doc2503_02",
+    categoria: "Documentos de Versões",
+    titulo: "Versão 25.03a - Clientes",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.03/25.03a",
+    arquivo: "versoes/25.03/25.03a/pdfs/25.03a_Clientes.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
+
+  {
+    id: "doc2503_03",
+    categoria: "Documentos de Versões",
+    titulo:
+      "Anexo I - Alteração na atualização do percentual aplicado ao preço de venda - Marketplace",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.03/25.03a",
+    arquivo:
+      "versoes/25.03/25.03a/Anexo I - Alteração na atualização do percentual aplicado ao preço de venda - Marketplace.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
+
     {
-        id: 'fin_01',
-        categoria: 'Módulo Financeiro',
-        titulo: 'Manual Tesouraria - Cadastro Básico',
-        tipo: 'pdf',
-        //arquivo: 'financeiro/pdfs/tesouraria_cadastro_basico.pdf',
-        arquivo: 'https://mega.nz/file/frZ22aCJ#Z2hcBHfLwA8cS9PtWMFG8vtqJgOJrMQmp3Ke6w9d-PY'
-        //descricao: 'Regras de descontos e prazos.'
-    },
+    id: "doc2503_04",
+    categoria: "Documentos de Versões",
+    titulo:
+      "Anexo II - Importação e utilização dos dados cadastrais de produtos da ABCFarma",
+    tipo: "pdf", // <--- DEFINE COMO MARKDOWN
+    pasta: "25.03/25.03a",
+    arquivo:
+      "versoes/25.03/25.03a/Anexo II - Importação e utilização dos dados cadastrais de produtos da ABCFarma.pdf",
+    //descricao: 'Como dar entrada via XML.'
+  },
+
+
+
+
+  //--------------------
+
+  // --- MÓDULO ESTOQUE ---
+  {
+    id: "est_01",
+    categoria: "Módulo Estoque",
+    titulo: "Entrada de Nota Fiscal",
+    tipo: "md", // <--- DEFINE COMO MARKDOWN
+    arquivo: "estoque/manual_entradaNFe.md",
+    descricao: "Como dar entrada via XML.",
+  },
+
+  // --- MÓDULO VENDAS ---
+  {
+    id: "vnd_01",
+    categoria: "Módulo Vendas",
+    titulo: "Política de Vendas",
+    tipo: "md",
+    arquivo: "vendas/politica_venda.md",
+    descricao: "Regras de descontos e prazos.",
+  },
+
+  // --- MÓDULO FINANCEIRO ---
+  {
+    id: "fin_01",
+    categoria: "Módulo Financeiro",
+    titulo: "Manual Tesouraria - Cadastro Básico",
+    tipo: "pdf",
+    //arquivo: 'financeiro/pdfs/tesouraria_cadastro_basico.pdf',
+    arquivo:
+      "https://mega.nz/file/frZ22aCJ#Z2hcBHfLwA8cS9PtWMFG8vtqJgOJrMQmp3Ke6w9d-PY",
+    //descricao: 'Regras de descontos e prazos.'
+  },
+
+  //25.03
+
 ];
